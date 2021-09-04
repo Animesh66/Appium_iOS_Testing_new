f044c323519a1b890f5c9856a7dc41319feafb3a
Animesh <er.animesh6@hotmail.com>
2021-09-04 22:52:04 +0530

    initial commit

--- test_iso_simulator.py
+++ test_iso_simulator.py
@@ -3,6 +3,9 @@ from appium import webdriver
 desired_cap = dict(
     platformName='iOS',
     platformVersion='14.5',
-    deviceName='Apple 12 Pro'
-
+    deviceName='iPhone 12 Pro',
+    udid='A97D4189-AB2F-490F-B1CC-FBABDC54259C',
+    bundleId='com.Animesh66.IntegrationApp'
 )
+
+driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_cap)
