4923a06705485d3560454e1d24fbb3bc88ba8d68
Animesh <er.animesh6@hotmail.com>
2021-09-05 22:18:04 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ test_iso_uiSliders.py
@@ -0,0 +1,19 @@
+import time
+from appium import webdriver
+
+desired_cap = dict(
+    platformName='iOS',
+    platformVersion='14.5',
+    deviceName='iPhone 12 Pro',
+    udid='A97D4189-AB2F-490F-B1CC-FBABDC54259C',
+    bundleId='com.Animesh.apple-samplecode.UICatalog',
+    noRest=True
+)
+
+driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_cap)
+driver.implicitly_wait(10)
+driver.find_element_by_accessibility_id('Sliders').click()
+element = driver.find_element_by_xpath("(//XCUIElementTypeStaticText[@name='0'])[1]")
+
+time.sleep(2)
+driver.quit()
