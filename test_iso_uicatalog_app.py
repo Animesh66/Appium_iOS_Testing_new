b93d6d6f67bba7c5999986862282d1bceb6a107f
Animesh <er.animesh6@hotmail.com>
2021-09-05 12:58:10 +0530

    Merge remote-tracking branch 'origin/master'

--- test_iso_uicatalog_app.py
+++ test_iso_uicatalog_app.py
@@ -12,9 +12,12 @@ desired_cap = dict(
 
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_cap)
 driver.implicitly_wait(10)
-driver.find_element_by_xpath("//XCUIElementTypeStaticText[@name='Alerts']").click()
-driver.find_element_by_xpath("//XCUIElementTypeStaticText[@name='Create Sheet Alert']").click()
-read_text = driver.find_element_by_xpath("//XCUIElementTypeStaticText[@name='Magic Sheet']").text
-print(read_text)
+driver.find_element_by_accessibility_id('Switches').click()
+default_switch = driver.find_elements_by_class_name('XCUIElementTypeSwitch')
+default_switch_value = default_switch.__getattribute__('value')
+print(default_switch_value)
+if default_switch_value == '1':
+    default_switch.click()
+print(default_switch_value)
 time.sleep(2)
 driver.quit()
