703f8fdef6e5d71c322e7d28e93dd126aef89a35
Animesh <er.animesh6@hotmail.com>
2021-09-05 11:56:18 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ test_iso_app_simulator.py
@@ -0,0 +1,16 @@
+import time
+from appium import webdriver
+
+desired_cap = dict(
+    platformName='iOS',
+    platformVersion='14.5',
+    deviceName='iPhone 12 Pro',
+    udid='A97D4189-AB2F-490F-B1CC-FBABDC54259C',
+    app='/Users/animeshmukherjee/PycharmProjects/pythonProject/Appium_iOS_Testing/app/simulator/IntegrationApp.app',
+    noRest=True
+)
+
+driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_cap)
+driver.implicitly_wait(10)
+time.sleep(5)
+driver.quit()
