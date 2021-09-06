8a92fd6fcca50bfe001fae56dca6895e9f803e19
Animesh <er.animesh6@hotmail.com>
2021-09-06 22:23:06 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ test_iso_scrolling_left_right.py
@@ -0,0 +1,26 @@
+import time
+from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
+
+desired_cap = dict(
+    platformName='iOS',
+    platformVersion='14.5',
+    deviceName='iPhone 12 Pro',
+    udid='A97D4189-AB2F-490F-B1CC-FBABDC54259C',
+    bundleId='com.Animesh66.IntegrationApp'
+)
+
+driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_cap)
+driver.implicitly_wait(10)
+element = driver.find_element_by_accessibility_id('Scrolling').click()
+driver.background_app(-1)
+
+action = TouchAction(driver)
+i = 0
+while not driver.find_element_by_accessibility_id('Settings').is_displayed():
+    action.press(x=24, y=172).wait(2000).move_to(x=285, y=172).perform()
+    i += 1
+driver.find_element_by_accessibility_id('Settings')
+driver.find_element_by_accessibility_id('Accessibility')
+time.sleep(2)
+driver.quit()
